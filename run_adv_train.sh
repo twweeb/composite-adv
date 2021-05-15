19f01d08fc33d0110b5ed9e1ab98a34ffa7c0006
Lei Hsiung <leihsiung.ray@gmail.com>
2021-05-15 20:38:01 +0800

    fixed untracked files

--- run_adv_train.sh
+++ run_adv_train.sh
@@ -4,4 +4,5 @@ python3 train_trades_cifar10_nodist.py --batch-size 128 --epochs 10 \
         --model-dir model_logs/adv_train_comp_5012_fixed/ \
         --log_filename ./result/logfile_train_comp_5012_fixed.csv \
         --order fixed --enable 5,0,1,2 \
-        --dist comp
+        --dist comp \
+        --model_path model_logs/adv_train_comp_5012_fixed/model_best.pth
