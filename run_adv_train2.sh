5721aea036aa8f06ada3cb508c4f4845bbc72cb1
Lei Hsiung <leihsiung.ray@gmail.com>
2021-05-17 00:28:26 +0800

    ignore other scripts

--- run_adv_train2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 train_trades_cifar10_nodist.py --batch-size 128 --epochs 10 \
-        --model-dir model_logs/adv_train_comp_012345_fixed/ \
-        --log_filename ./result/logfile_train_comp_012345_fixed.csv \
-        --order fixed --enable 0,1,2,3,4,5 \
-        --dist comp
--- run_example.sh
+++ run_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 train_trades_cifar10_nodist.py --batch-size 128 --epochs 10 \
+python3 train_trades_cifar10_nodist.py --batch-size 128 --epochs 20 \
         --model-dir model_logs/adv_train_comp_5012_fixed/ \
         --log_filename ./result/logfile_train_comp_5012_fixed.csv \
         --order fixed --enable 5,0,1,2 \
