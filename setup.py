72f3e1377cc2125afb18cf39fa35213283869eaa
Lei Hsiung <leihsiung.ray@gmail.com>
2022-02-11 17:18:12 +0800

    Package setup

--- setup.py
+++ setup.py
@@ -7,20 +7,22 @@ setuptools.setup(
     name="composite_adv",
     version="0.0.1",
     author="Lei Hsiung",
-    author_email="nthu.email@gmail.com",
+    author_email="hsiung@m109.nthu.edu.tw",
     description="Composite Adversarial Attack",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/twweeb/composite-adv",
     packages=setuptools.find_packages(),
     install_requires=[
+        'torch>=1.4.0',
+        'robustness>=1.1.post2',
+        'numpy>=1.16.0',
+        'torchvision>=0.5.0',
         'kornia>=0.6.1',
         'sinkhorn-knopp>=0.2',
-        'torch>=1.8.1',
-        'torchvision>=0.9.1',
         'scipy>=1.6.3',
-        'numpy>=1.20.3',
-        'requests>=2.27.1'
+        'requests>=2.23.0',
+        'advex-uar>=0.0.5.dev0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
